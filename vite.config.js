97c025f23dfae1993d3d10dd6a4b27726ea86691
sammy1208 <34804688+sammy1208@users.noreply.github.com>
2025-05-05 15:21:06 +0800

    重新佈署

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/202408week8/',
+  base: '/HTML-product-Tutoring/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
